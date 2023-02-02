0ba5c9f658e8f388cea4f633c045897fe47821aa
Urwik <f.soler@umh.es>
2023-02-02 09:10:27 +0100

    Update

--- scripts/main2.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-labels_normal = np.load('//arvc_utils/labels_normal_06162.npy')
-labels_bin = np.load('//arvc_utils/labels_bin_06162.npy')
-
-np.savetxt("original.csv", labels_normal, delimiter = ",")
-np.savetxt("bin.csv", labels_bin, delimiter = ",")
