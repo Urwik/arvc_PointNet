39124c3e8997e28a9f008dae619caa25efc08155
Fran Soler <f.soler@umh.es>
2023-07-13 13:47:29 +0200

    Udpate - Close to work correctly

--- binary_segmentation/scripts/trainer.py
+++ binary_segmentation/scripts/trainer.py
@@ -9,6 +9,7 @@ import numpy as np
 import sklearn.metrics as metrics
 
 import torch
+from torchsummary import summary
 from torch.utils.data import DataLoader
 
 import yaml
@@ -41,13 +42,47 @@ class Trainer():
         self.train_abs_path: str
         self.valid_abs_path: str
         self.output_dir: str
-        self.model: torch.nn.Module
         self.best_value: float
         self.last_value: float
+        self.activation_fn = self.configuration.train.activation_fn
+        self.device = self.configuration.train.device
+
+        self.epoch_timeout_count = 0
+
+        self.setup()
+
+
+    def setup(self):
+
+        self.make_outputdir()
+        self.save_config_file()
+        self.instantiate_dataset()
+        self.instantiate_dataloader()
+        self.set_device()
+        self.set_model()
+        self.set_optimizer()
+
+
+    def make_outputdir(self):
+        # --------------------------------------------------------------------------------------------#
+        # CREATE A FOLDER TO SAVE TRAINING
+        OUT_DIR = os.path.join(current_model_path, self.configuration.train.output_dir.__str__())
+
+        folder_name = datetime.today().strftime('%y%m%d%H%M%S')
+        OUT_DIR = os.path.join(OUT_DIR, folder_name)
+
+        if not os.path.exists(OUT_DIR):
+            os.makedirs(OUT_DIR)
+
+        self.output_dir = OUT_DIR
+
+
+    def save_config_file(self):
+        shutil.copyfile(self.configuration.config_path, os.path.join(self.output_dir, 'config.yaml'))
+
 
     def prefix_path(self):
-        # --------------------------------------------------------------------------------------------#
-        # CHANGE PATH DEPENDING ON MACHINE
+
         machine_name = socket.gethostname()
         prefix_path = ''
         if machine_name == 'arvc-fran':
@@ -57,30 +92,11 @@ class Trainer():
 
         return prefix_path
 
-    def make_outputdir(self):
-        # --------------------------------------------------------------------------------------------#
-        # CREATE A FOLDER TO SAVE TRAINING
-        folder_name = datetime.today().strftime('%y%m%d%H%M%S')
-
-        OUT_DIR = os.path.join(current_model_path, self.configuration.train.output_dir.__str__())
-        OUT_DIR = os.path.join(OUT_DIR, folder_name)
-
-        if not os.path.exists(OUT_DIR):
-            os.makedirs(OUT_DIR)
-
-        self.output_dir = OUT_DIR
-
-    def save_config_file(self):
-        shutil.copyfile(self.configuration.config_path, os.path.join(self.output_dir, 'config.yaml'))
-
-
 
     def instantiate_dataset(self):
-        # -------------------------------------------------------------------------------------------- #
-        # INSTANCE DATASET
 
         self.train_abs_path = os.path.join(self.prefix_path(), self.configuration.train.train_dir.__str__())
-        self.valid_abs_path = os.path.join(trainer.prefix_path(), self.configuration.train.valid_dir.__str__())
+        self.valid_abs_path = os.path.join(self.prefix_path(), self.configuration.train.valid_dir.__str__())
         
         self.train_dataset = PLYDataset(_mode='train',
                                    _root_dir= self.train_abs_path,
@@ -118,12 +134,36 @@ class Trainer():
 
 
     def set_device(self):
-        # ------------------------------------------------------------------------------------------------------------ #
-        # SELECT MODEL
         if torch.cuda.is_available():
             self.device = torch.device(self.configuration.train.device)
         else:
             self.device = torch.device("cpu")
+
+
+    def set_model(self):
+        add_len = 1 if self.configuration.train.add_range else 0
+        feat_len = len(self.configuration.train.feat_idx) + add_len
+
+        self.model = PointNetDenseCls(  k=self.configuration.train.output_classes,
+                                        n_feat=feat_len,
+                                        device=self.configuration.train.device.__str__()).to(self.configuration.train.device)
+        
+
+        pytorch_total_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+        print('-' * 50)
+        print(self.model.__class__.__name__)
+        print(f"N Parameters: {pytorch_total_params}\n\n")
+
+
+    def set_optimizer(self):
+        if self.configuration.train.optimizer == "adam":
+            return torch.optim.Adam(self.model.parameters(), lr=self.configuration.train.init_lr)
+        elif self.configuration.train.optimizer == "sgd":
+            return torch.optim.SGD(self.model.parameters(), lr=self.configuration.train.init_lr)
+        else:
+            print("WRONG OPTIMIZER")
+            exit()
+
 
 
     def save_results(self):
@@ -147,133 +187,111 @@ class Trainer():
         conf_matrix.append(np.mean(valid_results[4]))
         threshold.append(np.mean(valid_results[5]))
 
+
     def termination_criteria(self):
         if self.epoch_timeout_count > self.configuration.train.epoch_timeout:
             return True
         else:
             return False
 
+
     def improved_results(self):
-        # SAVE MODEL AND TEMINATION CRITERIA
-        if config.train.termination_criteria == "loss":
-            last_val = np.mean(valid_results[0])
-            if last_val < best_val:
-                torch.save(model.state_dict(), OUT_DIR + f'/best_model.pth')
-                best_val = last_val
-                self.epoch_timeout_count = 0
-            else:
-                self.epoch_timeout_count += 1
+        # LOSS
+        if self.configuration.train.termination_criteria == "loss":
+            self.last_value = np.mean(valid_results[0])
 
-        elif config.train.termination_criteria == "precision":
-            last_val = np.mean(valid_results[2])
-            if last_val > best_val:
-                torch.save(model.state_dict(), OUT_DIR + f'/best_model.pth')
-                best_val = last_val
-                self.epoch_timeout_count = 0
+            if self.last_value < self.best_value:
+                return True
             else:
-                self.epoch_timeout_count += 1
+                return False
 
-        elif config.train.termination_criteria == "f1_score":
-            last_val = np.mean(valid_results[1])
-            if last_val > best_val:
-                torch.save(model.state_dict(), OUT_DIR + f'/best_model.pth')
-                best_val = last_val
-                self.epoch_timeout_count = 0
+        # PRECISION    
+        elif self.configuration.train.termination_criteria == "precision":
+            self.last_value = np.mean(valid_results[2])
+            
+            if self.last_value > self.best_value:
+                return True
             else:
-                self.epoch_timeout_count += 1
+                return False
+
+        # F1 SCORE    
+        elif self.configuration.train.termination_criteria == "f1_score":
+            self.last_value = np.mean(valid_results[1])
+            
+            if self.last_value > self.best_value:
+                return True
+            else:
+                return False
+            
         else:
             print("WRONG TERMINATION CRITERIA")
             exit()
+
 
     def update_saved_model(self):
-        # SAVE MODEL AND TEMINATION CRITERIA
-        if config.train.termination_criteria == "loss":
-            last_val = np.mean(valid_results[0])
-            if last_val < best_val:
-                torch.save(model.state_dict(), OUT_DIR + f'/best_model.pth')
-                best_val = last_val
-                self.epoch_timeout_count = 0
-            else:
-                self.epoch_timeout_count += 1
 
-        elif config.train.termination_criteria == "precision":
-            last_val = np.mean(valid_results[2])
-            if last_val > best_val:
-                torch.save(model.state_dict(), OUT_DIR + f'/best_model.pth')
-                best_val = last_val
-                self.epoch_timeout_count = 0
-            else:
-                self.epoch_timeout_count += 1
-
-        elif config.train.termination_criteria == "f1_score":
-            last_val = np.mean(valid_results[1])
-            if last_val > best_val:
-                torch.save(model.state_dict(), OUT_DIR + f'/best_model.pth')
-                best_val = last_val
-                self.epoch_timeout_count = 0
-            else:
-                self.epoch_timeout_count += 1
+        if self.improved_results():
+            torch.save(self.model.state_dict(), self.output_dir + f'/best_model.pth')
+            self.best_value = self.last_value
+            self.epoch_timeout_count = 0
+            return True
         else:
-            print("WRONG TERMINATION CRITERIA")
-            exit()
+            self.epoch_timeout_count += 1
+            return False
 
-
-
-    def train(self, _device, _data_loader, _model, _loss_fn, _optimizer):
+       
+    def train(self):
         print('-'*50 + '\n' + 'TRAINING' + '\n' + '-'*50)
-        
-        loss_lst = []
         current_clouds = 0
-
+        
         self.model.train()
 
-        for batch, (data, label, _) in enumerate(_data_loader):
-            data = data.to(_device, dtype=torch.float32)
-            label = label.to(_device, dtype=torch.float32)
-            
-            # Evaluate model
-            pred, m3x3, m64x64 = _model(data.transpose(1, 2))
+        for batch, (_, data, label, _) in enumerate(self.train_dataloader):
+            self.set_optimizer().zero_grad()
 
-            # Activation function
-            m = torch.nn.Sigmoid()
-            pred = m(pred)
+            data = data.to(self.device, dtype=torch.float32)
+            label = label.to(self.device, dtype=torch.float32)
+           
+            # Evaluate model
+            pred, m3x3, m64x64 = self.model(data.transpose(1, 2))
+
+            pred = self.activation_fn(pred)
 
             # Calulate loss and backpropagate
-            avg_train_loss_ = _loss_fn(pred, label)
-            _optimizer.zero_grad()
+            avg_train_loss_ = self.configuration.train.loss_fn(pred, label)
             avg_train_loss_.backward()
-            _optimizer.step()
-
-            # Save loss for plotting
-            loss_lst.append(avg_train_loss_.item())
+            self.set_optimizer().step()
 
             # Print training progress
             current_clouds += data.size(0)
-            if batch % 10 == 0 or data.size(0) < _data_loader.batch_size:  # print every (% X) batches
-                print(f' - [Batch: {current_clouds}/{len(_data_loader.dataset)}],'
+            if batch % 10 == 0 or data.size(0) < self.train_dataloader.batch_size:  # print every (% X) batches
+                print(f' - [Batch: {current_clouds}/{len(self.train_dataloader.dataset)}],'
                     f' / Train Loss: {avg_train_loss_:.4f}')
 
-        return loss_lst
 
-
-    def valid(_device, dataloader_, _model, _loss_fn):
+    def valid(self):
         print('-'*50 + '\n' + 'VALIDATION' + '\n' + '-'*50)
-
-        _model.eval()
-        f1_lst, pre_lst, rec_lst, loss_lst, conf_m_lst, trshld_lst = [], [], [], [], [], []
         current_clouds = 0
 
-        with torch.no_grad():
-            for batch, (data, label, _) in enumerate(dataloader_):
-                data, label = data.to(_device, dtype=torch.float32), label.to(_device, dtype=torch.float32)
-                pred, m3x3, m64x64 = _model(data.transpose(1, 2))
-                m = torch.nn.Sigmoid()
-                pred = m(pred)
+        self.model.eval()
 
-                avg_loss = _loss_fn(pred, label)
+        f1_lst, pre_lst, rec_lst, loss_lst, conf_m_lst, trshld_lst = [], [], [], [], [], []
+
+        with torch.no_grad():
+            for batch, (_, data, label, _) in enumerate(self.valid_dataloader):
+                data = data.to(self.device, dtype=torch.float32)
+                label = label.to(self.device, dtype=torch.float32)
+                
+                pred, m3x3, m64x64 = self.model(data.transpose(1, 2))
+
+                pred = self.activation_fn(pred)
+
+                avg_loss = self.configuration.train.loss_fn(pred, label)
                 loss_lst.append(avg_loss.item())
 
                 trshld, pred_fix, avg_f1, avg_pre, avg_rec, conf_m = validation_metrics(label, pred)
+
+
                 trshld_lst.append(trshld)
                 f1_lst.append(avg_f1)
                 pre_lst.append(avg_pre)
@@ -282,8 +300,8 @@ class Trainer():
 
                 current_clouds += data.size(0)
 
-                if batch % 10 == 0 or data.size()[0] < dataloader_.batch_size:  # print every 10 batches
-                    print(f'[Batch: {current_clouds}/{len(dataloader_.dataset)}]'
+                if batch % 10 == 0 or data.size()[0] < self.valid_dataloader.batch_size:  # print every 10 batches
+                    print(f'[Batch: {current_clouds}/{len(self.valid_dataloader.dataset)}]'
                         f'  [Avg Loss: {avg_loss:.4f}]'
                         f'  [Avg F1 score: {avg_f1:.4f}]'
                         f'  [Avg Precision score: {avg_pre:.4f}]'
@@ -292,69 +310,77 @@ class Trainer():
         return loss_lst, f1_lst, pre_lst, rec_lst, conf_m_lst, trshld_lst
 
 
-    
+
+class Results:
+    def __init__(self):
+        self.f1 = []
+        self.precision = []
+        self.recall = []
+        self.conf_matrix = []
+        self.train_loss = []
+        self.valid_loss = []
+        self.threshold = []
+
+    def add_train(self, results):
+        self.f1.append(results[0])
+        self.precision.append(results[1])
+        self.recall.append(results[2])
+        self.conf_matrix.append(results[3])
+        self.train_loss.append(results[4])
+        self.valid_loss.append(results[5])
+        self.threshold.append(results[6])
+
+    def add_valid(self, results):
+        print(results)
+
+
+
+def get_config(_config_file):
+    config_file_abs_path = os.path.join(current_model_path, 'config', _config_file)
+    config = cfg(config_file_abs_path)
+
+    return config
+
+
 
 if __name__ == '__main__':
 
     # Files = os.listdir(os.path.join(current_model_path, 'config'))
-    Files = ['config_xyz_0.yaml']
+    Files = ['config_new_version.yaml']
     for configFile in Files:
-        start_time = datetime.now()
+        training_start_time = datetime.now()
 
-        # --------------------------------------------------------------------------------------------#
-        # GET CONFIGURATION PARAMETERS
-        CONFIG_FILE = configFile
-        config_file_abs_path = os.path.join(current_model_path, 'config', CONFIG_FILE)
-        config = cfg(config_file_abs_path)
+        config = get_config(configFile)
 
         trainer = Trainer(config)
 
-        model = PointNetDenseCls(k=config.train.output_classes,
-                                 n_feat=len(config.train.feat_idx) + 1 if config.train.add_range else 0,
-                                 device=device).to(device)
-        
-        loss_fn = torch.nn.BCELoss()
-
-        optimizer = torch.optim.Adam(model.parameters(), lr=config.train.init_lr)
-
-
+        global_results = Results()
 
         # ------------------------------------------------------------------------------------------------------------ #
         # --- TRAIN LOOP --------------------------------------------------------------------------------------------- #
-        print('TRAINING ON: ', trainer.device)
-        epoch_timeout_count = 0
+        print('TRAINING ON: ', trainer.device.__str__())
 
-        best_val = 1 if trainer.configuration.train.termination_criteria == "loss" else 0:
+        trainer.best_value = 1 if config.train.termination_criteria == "loss" else 0
 
-        f1, precision, recall, conf_matrix, train_loss, valid_loss, threshold = [], [], [], [], [], [], []
 
         for epoch in range(config.train.epochs):
             print(f"EPOCH: {epoch} {'-' * 50}")
             epoch_start_time = datetime.now()
 
+            # update_lr(trainer.optimizer, epoch, config.train.lr_decay, config.train.init_lr)
+
             train_results = trainer.train() 
-            train(_device=device, data_loader=train_dataloader,_model_=model, loss_fn=loss_fn,_optimizer=optimizer)
+            valid_results = trainer.valid()
 
-            valid_results = valid(_device=device,_data_loader=valid_dataloader,_model_=model, loss_fn=loss_fn,_optimizer=optimizer)
+            global_results.add_train(train_results)
+            global_results.add_valid(valid_results)
 
-            # GET RESULTS
-            train_loss.append(train_results)
-
-            valid_loss.append(valid_results[0])
-            f1.append(valid_results[1])
-            precision.append(valid_results[2])
-            recall.append(valid_results[3])
-            conf_matrix.append(valid_results[4])
-            threshold.append(valid_results[5])
 
             print('-'*50 + '\n' + 'DURATION:' + '\n' + '-'*50)
             epoch_end_time = datetime.now()
             print('Epoch Duration: {}'.format(epoch_end_time-epoch_start_time))
 
 
-
-
-
-        end_time = datetime.now()
-        print('Total Training Duration: {}'.format(end_time-start_time))
+        training_end_time = datetime.now()
+        print('Total Training Duration: {}'.format(training_end_time-training_start_time))
         print("Training Done!")
